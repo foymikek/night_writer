c61bc187db7c2efb97a4dd72838d3ec243f0d227
Michael Foy <foymikek@gmail.com>
2020-11-09 08:17:01 -0700

    Add night_writer.rb runner code

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,14 +1,3 @@
-handle = File.open(ARGV[0], "r")
+require "./lib/file_io"
 
-incoming_text = handle.read
-
-handle.close
-
-cap_text = incoming_text.upcase
-
-writer = File.open(ARGV[1], "w")
-
-writer.write(cap_text)
-
-writer.close
-puts "Created  #{'ARVG[1]'} containing #{cap_text.count} characters"
+eng_to_braille = FileIo.new(ARGV[0], ARGV[1])
