75279fe2c91f7ce90ea1b7a9811638b552587ea2
Michael Foy <foymikek@gmail.com>
2020-11-09 08:42:18 -0700

    Add read method

--- lib/file_io.rb
+++ lib/file_io.rb
@@ -1,8 +1,16 @@
 class FileIo
   attr_reader :input, :output
-  
+
   def initialize(input, output)
     @input = input
     @output = output
   end
+
+  def read
+    handle = File.open(@input, "r")
+    raw_text = handle.read.chomp
+    handle.close
+    @input = raw_text
+  end
+
 end
--- test/file_io_test.rb
+++ test/file_io_test.rb
@@ -8,4 +8,12 @@ class FileIoTest < Minitest::Test
     assert_equal "abc", file_io.input
     assert_equal "cba", file_io.output
   end
+
+  def test_file_io_can_read_txt_file
+    input = "message_test1.txt"
+    output   = "dummy_test_file.txt"
+    file_io = FileIo.new(input, output)
+    file_io.read
+    assert_equal "this is saved to my input attribute", file_io.input
+  end
 end
