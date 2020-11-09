ec72fbdc4f3d984228728f5a411ddabd1662b952
Michael Foy <foymikek@gmail.com>
2020-11-09 15:01:28 -0700

    Add test_it_exists_and_has_attributes

--- lib/braille_file_io.rb
+++ lib/braille_file_io.rb
@@ -5,4 +5,16 @@ class BrailleFileIo
     @input = input
     @output = output
   end
+
+  def read
+    handle = File.open(@input, "r")
+    raw_braille = handle.read.chomp
+    handle.close
+    @input = raw_braille
+  end
+
+  def organize_message
+    text = (BrailleFileIo.new.translate(@input))
+    text.scan(/.{1,80}/)+"\n"
+  end
 end
