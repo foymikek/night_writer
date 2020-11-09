c55e1d7debe7d99a5a12408eddb48e763e152106
Michael Foy <foymikek@gmail.com>
2020-11-09 14:20:29 -0700

    Add night_reader.rb runner

--- lib/braille_file_io.rb
+++ lib/braille_file_io.rb
@@ -5,4 +5,18 @@ class BrailleFileIo
     @input = input
     @output = output
   end
+
+  def read
+    handle = File.open(@input, "r")
+    raw_text = handle.read.chomp
+    handle.close
+    @input = raw_text
+  end
+
+  def write
+    output = File.open(@output, "w")
+    count = output.write(BrailleTranslator.new.braille_translator(@input_text))
+    puts "Created #{@output} containing #{count} characters."
+    output.close
+  end
 end
---
+++ lib/night_reader.rb
@@ -0,0 +1,3 @@
+require "./lib/braille_file_io"
+
+braille_to_english  = BrailleFileIo.new(ARGV[0] , ARGV[1])
